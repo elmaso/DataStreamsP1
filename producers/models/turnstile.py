177489a03b94d73101a12127f8039dff7c9f08de
maso <elmaso@gmail.com>
2021-12-05 13:48:17 -0800

    Step 1 Done

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -30,6 +30,7 @@ class Turnstile(Producer):
             .replace("-", "_")
             .replace("'", "")
         )
+        topic_prefix = "org.chicago.cta.turnstile"
 
         #
         #
@@ -38,11 +39,11 @@ class Turnstile(Producer):
         #
         #
         super().__init__(
-            f"{station_name}", # TODO: Come up with a better topic name
+            f"{topic_prefix}", # TODO: Come up with a better topic name
             key_schema=Turnstile.key_schema,
-            # TODO: value_schema=Turnstile.value_schema, TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Turnstile.value_schema, #TODO: Uncomment once schema is defined
+            num_partitions=1,  # Setting to 1 wile is in localmode
+            num_replicas=1,  # Setting to 1 wile is in localmode
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
@@ -50,10 +51,22 @@ class Turnstile(Producer):
     def run(self, timestamp, time_step):
         """Simulates riders entering through the turnstile."""
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
-        logger.info("turnstile kafka integration incomplete - skipping")
+        # logger.info("turnstile kafka integration incomplete - skipping")
         #
         #
         # TODO: Complete this function by emitting a message to the turnstile topic for the number
         # of entries that were calculated
         #
         #
+        for _ in range(num_entries):
+            self.producer.produce(
+                topic=self.topic_name,
+                key_schema=Turnstile.key_schema,
+                value_schema=Turnstile.value_schema,
+                key={"timestamp": self.time_millis()},
+                value={
+                    "station_id": self.station.station_id,
+                    "station_name": self.station.name,
+                    "line": self.station.color.name
+                },
+            )
